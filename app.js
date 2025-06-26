ae2ee8a1b408f6492ebb9c5ec98804d855a9b5b4
Kishorelin03 <Kishorelin.tad001@gmail.com>
2025-06-25 23:15:39 -0400

    Adding audio to the pads

--- app.js
+++ app.js
@@ -31,6 +31,23 @@ class Drumkit {
         // Select all pads that belong to this step across all tracks
         const activeBars = document.querySelectorAll(`.b${step}`);
 
+        activeBars.forEach( bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            if(bar.classList.contains('active')){
+                if(bar.classList.contains('kick-pad')){
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if(bar.classList.contains('snare-pad')){
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if(bar.classList.contains('hihat-pad')){
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         // Increment step for next iteration
         this.index++;
     }
@@ -53,6 +70,9 @@ const drumKit = new Drumkit();
 // Add click event listeners to all pads to toggle their active state
 drumKit.pads.forEach(pad => {
     pad.addEventListener("click", drumKit.activePad);
+    pad.addEventListener("animationend", function(){
+        this.style.animation = "";
+    })
 });
 
 // Add event listener to the play button to start the loop
